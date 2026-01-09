e3847b601a006c7f8950289a1057848c7fa29a90
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-09 16:50:14 +0100

    updated shell.c

--- shell.c
+++ shell.c
@@ -7,42 +7,34 @@
 int main(void)
 {
 	size_t len = 0;
-	char *line = NULL, *path_cmd;
+	char *line = NULL, *path_cmd, **argv;
 
 	while (1)
 	{
-		/* 1. afficher le prompt */
-		if (isatty(STDIN_FILENO))
-		{
-		write(STDOUT_FILENO, "$ ", 2);
-		}
+		if (isatty(STDIN_FILENO)) /* 1. afficher le prompt */
+			write(STDOUT_FILENO, "$ ", 2);
 
-		/* 2. lire la commande */
-		if (getline(&line, &len, stdin) == -1)
+		if (getline(&line, &len, stdin) == -1) /* 2. lire la commande */
 		{
-			/* Ctrl + D (EOF) */
-			if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "\n", 1);
+			if (isatty(STDIN_FILENO)) /* Ctrl + D (EOF) */
+				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
 
-		/* 3. enlever le '\n' */
-		line[strcspn(line, "\n")] = '\0';
+		line[strcspn(line, "\n")] = '\0'; /* 3. enlever le '\n' */
 
-		/* 4. si ligne vide, afficher à nouveau le prompt */
-		if (line[0] == '\0')
+		if (line[0] == '\0') /* 4. si ligne vide, afficher à nouveau le prompt */
 			continue;
+
+		argv = tokenizer(line);
+
+		if (argv[0] != NULL) /* Si le nom contient un slash direct */
 		{
-
-			char **argv = tokenizer(line);
-
-			if (argv[0] != NULL) /* Si le nom contient un slash direct */
-			{
-				if (strcmp (argv[0], "exit") == 0)
+			if (strcmp(argv[0], "exit") == 0)
 				break;
 
-				if (strchr(argv[0], '/') != NULL)
-					path_cmd = strdup(argv[0]); /* chemin donné directement*/
+			if (strchr(argv[0], '/') != NULL)
+				path_cmd = strdup(argv[0]); /* chemin donné directement*/
 			else
 				path_cmd = find_in_path(argv[0]);
 
@@ -53,8 +45,7 @@ int main(void)
 				free(path_cmd);
 			}
 		}
-			free(argv);
-		}
+		free(argv);
 	}
 	free(line);
 	return (0);
