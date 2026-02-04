61795fb59b86cfec3cf7446e1d0e60f0c56c48fb
ValPQT <valentinpasquiet@gmail.com>
2026-02-04 17:49:31 +0100

    fix : update commentary

--- shell.c
+++ shell.c
@@ -21,7 +21,7 @@ int main(void)
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))/** ceci affiche le prompt */
+		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "$ ", 2);
 
 		if (getline(&line, &len, stdin) == -1)
